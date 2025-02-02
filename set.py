7fa7ba71954c766ba35ff2f98cac100fbfe8d9d3
kmavit <kimavit@gmail.com>
2025-02-02 22:18:11 +0300

    set fruit

---
+++ set.py
@@ -0,0 +1,5 @@
+fruits = {'apple', 'banana', 'cherry', 'avocado', 'pineapple', 'apricot', 'banana', 'avocado', 'grapefruit'}
+sorted_fruits = sorted(fruits, reverse=True)
+
+for fruit in sorted_fruits:
+    print(fruit)
